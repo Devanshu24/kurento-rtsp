c749fb86e8e1dc7e82ad575758ccf127e3828749
blueblackzebra <sparshkasana99@gmail.com>
2020-06-11 18:41:18 +0530

    Temporary fix

--- index.js
+++ index.js
@@ -55,75 +55,79 @@ io.on('connection', (socket)=> {
     
     //console.log(kurentoClient)
 
-    kurentoClient.create('MediaPipeline', function(error, pipeline) {
-        if (error){
-            return console.log(error);
-        }
-
-        // console.log(pipeline)
-        createMediaElems(pipeline, function(error, webRtc){
-            if (error){
-                return console.log(error)
-            }
-
-            if (queue){
-                console.log(queue + '\n' + "queue")
-                while (queue.length){
-                    let cand = queue.shift();
-                    webRtc.addIceCandidate(cand)
-                }
-            }
-
-            connectMediaElems(webRtc, function(error, webRtcalt){
-                if (error){
-                    pipeline.release();
-                }
-
-                webRtcalt.on('OnIceCandidate', function(event) {
-                    let candidate = kurento.getComplexType('IceCandidate')(event.candidate);
-                    socket.emit('finalice', candidate)
-                })
-
-                webRtcEndpoint=webRtcalt;
-                pipe = pipeline;
-
-            })   
-            
-        });
-            
-    });
-
-    setTimeout(() => {
-        console.log(pipe);
-        console.log("------------------------")
-        console.log(webRtcEndpoint)
+    // setTimeout(() => {
+    //     console.log(pipe);
+    //     console.log("------------------------")
+    //     console.log(webRtcEndpoint)
         
-    }, 1000);
+    // }, 1000);
 
     socket.on('sdpOffer', (offer) => {
-        webRtcEndpoint.processOffer(offer, function(error, answer) {
+        kurentoClient.create('MediaPipeline', function(error, pipeline) {
             if (error){
-                pipeline.release()
-                console.log(error);
+                return console.log(error);
             }
-
-            socket.emit('sdpAnswer', answer)
-
-            webRtcEndpoint.gatherCandidates((error) => {
+    
+            // console.log(pipeline)
+            createMediaElems(pipeline, function(error, webRtc){
                 if (error){
-                    console.log(error)
+                    return console.log(error)
                 }
-            })
+    
+                if (queue){
+                    console.log(queue + '\n' + "queue")
+                    while (queue.length){
+                        let cand = queue.shift();
+                        webRtc.addIceCandidate(cand)
+                    }
+                }
+    
+                connectMediaElems(webRtc, function(error, webRtcalt){
+                    if (error){
+                        pipeline.release();
+                    }
+    
+                    webRtcalt.on('OnIceCandidate', function(event) {
+                        let candidate = kurento.getComplexType('IceCandidate')(event.candidate);
+                        socket.emit('finalice', candidate)
+                    })
+
+                    webRtcalt.processOffer(offer, function(error, answer) {
+                        if (error){
+                            pipeline.release()
+                            console.log(error);
+                        }
             
-        })
+                        socket.emit('sdpAnswer', answer)
+            
+                        webRtcalt.gatherCandidates((error) => {
+                            if (error){
+                                console.log(error)
+                            }
+                        })
+                        
+                    })
+    
+                    webRtcEndpoint=webRtcalt;
+                    pipe = pipeline;
+    
+                })   
+                
+            });
+                
+        });
+
+        
     })
 
     socket.on('initice', (cand) => {
         let candidate = kurento.getComplexType('IceCandidate')(cand);
-        console.log(cand);
-        console.log(candidate)
+        // console.log(cand);
+        // console.log(candidate)
 
-        console.log(queue +'\n' + "queue")
+        console.log(queue +'\n' + "q")
+
+        console.log(webRtcEndpoint)
 
         if (webRtcEndpoint){
             webRtcEndpoint.addIceCandidate(candidate);
@@ -131,13 +135,10 @@ io.on('connection', (socket)=> {
         else {
             queue.push(candidate);
         }
-        
-        // queue.push(candidate)
 
-        // webRtcEndpoint.on('OnIceCandidate', function(event) {
-        //     let candidate = kurento.getComplexType('IceCandidate')(event.candidate);
-        //     socket.emit('finalice', candidate)
-        // })
+        console.log(queue + '\n' + "qu")
+
+        
         // socket.emit('finalice', candidate);
     })
 
